Figured out a bunch of problem - 1. Add sample and loss weighting strategies 2. Change loss functions 3.Fixed input scaling problem 4. Reduce underfitting

data.py
@@ -4,12 +4,19 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import h5py
 
+
 class WildFireData(Dataset):
     def __init__(self, data_path, target_hour, obsv_tagt_transforms, land_transforms, meteo_transforms):
         self.data = {}
         with h5py.File(data_path, 'r') as f:
             for k in list(f):
                 self.data[k] = f[k][:]
+
+        # limit = 1000
+        # self.data['observed'] = self.data['observed'][limit:limit+1000]
+        # self.data['target'] = self.data['target'][limit:limit+1000]
+        # self.data['land_cover'] = self.data['land_cover'][limit:limit+1000]
+        # self.data['meteorology'] = self.data['meteorology'][limit:limit+1000]
 
         self.data['observed'] = self.data['observed']
         self.data['target'] = self.data['target']
@@ -26,10 +33,16 @@ class WildFireData(Dataset):
 
     def __getitem__(self, idx):
         sample_observed = self.data['observed'][idx]
+        sample_observed[sample_observed == 1] = 255
+
         if self.target_hour == 12:
-            sample_target = self.data['target'][idx, 0][np.newaxis, :]
+            sample_target = self.data['target'][idx, 0]
+            sample_target[sample_target == 1] = 255
+            sample_target = sample_target[np.newaxis, :]
         else:
-            sample_target = self.data['target'][idx, 1][np.newaxis, :]
+            sample_target = self.data['target'][idx, 1]
+            sample_target[sample_target == 1] = 255
+            sample_target = sample_target[np.newaxis, :]
             sample_observed = np.concat([sample_observed, self.data['target'][idx, 0]])
 
         sample_land_cover = self.data['land_cover'][idx]
@@ -50,11 +63,12 @@ class WildFireData(Dataset):
         sample_target = self.obsv_tagt_transforms(sample_target)
         sample_land_cover = self.land_transforms(sample_land_cover)
         sample_meteorology = self.meteo_transforms(sample_meteorology)
+        sample_weight = sample_observed.sum() / 100 + 0.1
 
         X = torch.cat([sample_observed, sample_land_cover, sample_meteorology.float()], dim=0)
         y = sample_target
 
-        return X, y
+        return X, y, sample_weight
 
 
 def get_wild_fire_data(data_path, target_hour):
@@ -65,14 +79,14 @@ def get_wild_fire_data(data_path, target_hour):
     return WildFireData(data_path, target_hour, obsv_tagt_transforms, land_transforms, meteo_transforms)
 
 
-def get_data_loader(batch_size, target_hour):
+def get_data_loader(batch_size, target_hour, shuffle_training=True):
     train_data_path = 'uci_ml_hackathon_fire_dataset_2012-05-09_2013-01-01_10k_train.hdf5'
     train_data = get_wild_fire_data(train_data_path, target_hour)
 
     test_data_path = 'uci_ml_hackathon_fire_dataset_2013-01-01_2014-01-01_5k_test.hdf5'
     test_data = get_wild_fire_data(test_data_path, target_hour)
 
-    train_data_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    train_data_loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle_training)
     test_data_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
     return train_data_loader, test_data_loader

models.py
@@ -57,18 +57,18 @@ class GeneratorUNet(nn.Module):
         super(GeneratorUNet, self).__init__()
 
         self.down1 = UNetDown(in_channels, 256, normalize=False)
-        self.down2 = UNetDown(256, 512, dropout=0.5)
-        self.down3 = UNetDown(512, 512, dropout=0.5)
-        self.down4 = UNetDown(512, 512, normalize=False, dropout=0.5)
+        self.down2 = UNetDown(256, 512, dropout=0.25)
+        self.down3 = UNetDown(512, 512, dropout=0.25)
+        self.down4 = UNetDown(512, 512, normalize=False, dropout=0.25)
 
-        self.up1 = UNetUp(512, 512, dropout=0.5)
-        self.up2 = UNetUp(1024, 512, dropout=0.5)
+        self.up1 = UNetUp(512, 512, dropout=0.25)
+        self.up2 = UNetUp(1024, 512, dropout=0.25)
         self.up3 = UNetUp(1024, 256)
 
         self.final = nn.Sequential(
             nn.Upsample(scale_factor=2),
             nn.Conv2d(512, out_channels, 3, padding=1),
-            nn.Sigmoid(),
+            # nn.Sigmoid(),
         )
 
     def forward(self, x):

pix2pix.py
@@ -8,14 +8,15 @@ import torch
 from models import weights_init_normal, GeneratorUNet, Discriminator
 from data import get_data_loader
 from utils import set_fig_settings, FIG_REG_ASPECT_RATIO, FIG_REG_WIDTH
+from tensorboardX import SummaryWriter
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
-parser.add_argument("--n_epochs", type=int, default=50, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=500, help="number of epochs of training")
 parser.add_argument("--dataset_name", type=str, default="wildfires", help="name of the dataset")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--lr", type=float, default=0.0005, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.75, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to start lr decay")
 parser.add_argument("--n_cpu", type=int, default=4, help="number of cpu threads to use during batch generation")
@@ -25,7 +26,7 @@ parser.add_argument('--target_hour', type=int, default=12, help="target hour to 
 parser.add_argument(
     "--sample_interval", type=int, default=500, help="interval between sampling of images from generators"
 )
-parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between model checkpoints")
+parser.add_argument("--checkpoint_interval", type=int, default=5, help="interval between model checkpoints")
 args = parser.parse_args()
 print(args)
 
@@ -33,15 +34,20 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 os.makedirs("saved_models/%s" % args.dataset_name, exist_ok=True)
 os.makedirs("sample_images/%s" % args.dataset_name, exist_ok=True)
+os.makedirs("logs/%s" % args.dataset_name, exist_ok=True)
+writer = SummaryWriter("logs/%s" % args.dataset_name)
 
 cuda = True if torch.cuda.is_available() else False
 
 # Loss functions
-criterion_GAN = torch.nn.MSELoss()
-criterion_pixelwise = torch.nn.L1Loss()
+# criterion_GAN = torch.nn.MSELoss(reduction='none')
+criterion_GAN = torch.nn.BCEWithLogitsLoss(reduction='none')
 
+# criterion_pixelwise = torch.nn.MSELoss(reduction='none')
+criterion_pixelwise = torch.nn.BCEWithLogitsLoss(reduction='none', pos_weight=torch.full((30, 30), 10))
 # Loss weight of L1 pixel-wise loss between translated image and real image
-lambda_pixel = 100
+lambda_pixel = 1
+lambda_GAN = 1
 
 # Calculate output of image discriminator (PatchGAN)
 patch = (1, args.img_height // 2 ** 2, args.img_width // 2 ** 2)
@@ -79,14 +85,13 @@ def sample_images(batches_done):
     real_B = imgs[1].to(device)
     fake_B = generator(real_A)
 
-    img_dir =  "sample_images/%s/%s.png" % (args.dataset_name, batches_done)
-
+    img_dir = "sample_images/%s/%s.png" % (args.dataset_name, batches_done)
     set_fig_settings((FIG_REG_WIDTH * 2, FIG_REG_WIDTH * 1.25))
     fig = plt.figure()
     for i in range(2):
         plt.subplot(1, 2, i + 1)
         plt.title('Real') if i == 0 else plt.title('Fake')
-        plt.imshow([real_B[0,0].cpu().detach().numpy(), fake_B[0,0].cpu().detach().numpy()][i])
+        plt.imshow([real_B[0, 0].cpu().detach().numpy(), fake_B[0, 0].cpu().detach().numpy()][i])
         plt.axis('off')
     plt.tight_layout()
     plt.savefig(img_dir)
@@ -103,6 +108,8 @@ for epoch in range(args.epoch, args.n_epochs):
         # Model inputs
         real_A = batch[0].to(device)
         real_B = batch[1].to(device)
+        sample_weights = batch[2].view(real_A.size(0), 1, 1, 1).to(device)
+        # sample_weights = torch.full_like(sample_weights, 1, device=device)
 
         # Adversarial ground truths
         valid = torch.full((real_A.size(0), *patch), 1, device=device)
@@ -117,12 +124,15 @@ for epoch in range(args.epoch, args.n_epochs):
         # GAN loss
         fake_B = generator(real_A)
         pred_fake = discriminator(fake_B, real_A)
-        loss_GAN = criterion_GAN(pred_fake, valid)
+        loss_GAN = criterion_GAN(pred_fake, valid) * sample_weights
+        loss_GAN = loss_GAN.mean()
+
         # Pixel-wise loss
-        loss_pixel = criterion_pixelwise(fake_B, real_B)
+        loss_pixel = criterion_pixelwise(fake_B, real_B) * sample_weights
+        loss_pixel = loss_pixel.mean()
 
         # Total loss
-        loss_G = loss_GAN + lambda_pixel * loss_pixel
+        loss_G = lambda_GAN * loss_GAN + lambda_pixel * loss_pixel
 
         loss_G.backward()
 
@@ -136,11 +146,13 @@ for epoch in range(args.epoch, args.n_epochs):
 
         # Real loss
         pred_real = discriminator(real_B, real_A)
-        loss_real = criterion_GAN(pred_real, valid)
+        loss_real = criterion_GAN(pred_real, valid) * sample_weights
+        loss_real = loss_real.mean()
 
         # Fake loss
         pred_fake = discriminator(fake_B.detach(), real_A)
-        loss_fake = criterion_GAN(pred_fake, fake)
+        loss_fake = criterion_GAN(pred_fake, fake) * sample_weights
+        loss_fake = loss_fake.mean()
 
         # Total loss
         loss_D = 0.5 * (loss_real + loss_fake)
@@ -165,6 +177,11 @@ for epoch in range(args.epoch, args.n_epochs):
                loss_D.item(), loss_G.item(), loss_pixel.item(), loss_GAN.item(),
                time_left)
         )
+
+        writer.add_scalars('losses', {'g_loss': loss_G.item()}, batches_done)
+        writer.add_scalars('losses', {'d_loss': loss_D.item()}, batches_done)
+        writer.add_scalars('losses', {'gan_loss': loss_GAN.item()}, batches_done)
+        writer.add_scalars('losses2', {'pixel_loss': loss_pixel.item()}, batches_done)
 
         # If at sample interval save image
         if batches_done % args.sample_interval == 0: